038432adb454bb4031c5defb061bae27ce1f9b99
bluegrass322 <79901594+bluegrass322@users.noreply.github.com>
2024-04-03 10:21:16 +0900

    Update autocmds.lua

--- lazy_vim/lua/config/autocmds.lua
+++ lazy_vim/lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+local autocmd = vim.api.nvim_create_autocmd
+--vim.api.nvim_create_augroup('lua', {})
+autocmd('TermOpen', {
+  --group = 'lua',
+  callback = function()
+    vim.cmd "startinsert"
+  end
+})
+
+local cmd = vim.api.nvim_create_user_command
+cmd("W", "w", {})
+cmd("Q", "q", {})
+cmd("WQ", "wq", {})
