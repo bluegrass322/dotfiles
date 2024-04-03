1caca9766be980def6bc1cedfbecde3b6f39fcfd
bluegrass322 <79901594+bluegrass322@users.noreply.github.com>
2024-04-03 10:29:06 +0900

    Create colorscheme.lua

---
+++ lazy_vim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  { "RRethy/base16-nvim" },
+  { "keiyakeiya/PapilioDehaanii.vim" },
+  { "Badacadabra/vim-archery" },
+  { "AndrewLockVI/dark_ocean.vim" },
+  { "oahlen/iceberg.nvim" },
+  { "neg-serg/neg.nvim" },
+  { "lunacookies/vim-substrata" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "iceberg",
+    },
+  },
+}
