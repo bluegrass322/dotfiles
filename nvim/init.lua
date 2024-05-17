a67ad647c78a1313444c180c4113584691dc626b
bluegrass322 <79901594+bluegrass322@users.noreply.github.com>
2024-05-17 11:58:35 +0900

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,6 @@ require("netrw")
 require("options")
 
 require("lazy_nvim")
-vim.cmd("colorscheme iceberg")
 
 -- Prepend mise shims to PATH
 vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
