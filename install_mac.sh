77c6cab44e15e1320008038d3eaa0058a5698d3f
bluegrass322 <79901594+bluegrass322@users.noreply.github.com>
2024-05-17 12:01:52 +0900

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
-DOT_DIR="$HOME/dotfiles"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle --file=./Brewfile
 
-for file in .gitconfig, .zshrc;
+DOT_DIR="$HOME/dotfiles"
+for file in .gitconfig, .zshrc, .zprofile;
 do
     ln -snf $DOT_DIR/"$file" $HOME/".$file"
-    echo "Created .$file"
+    echo "Linked .$file"
 done
 
-brew install asdf
-echo "Installed $(asdf --version)"
-
-brew install git
-echo "Installed $(git version)"
-
-brew install neovim
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 # symbolic link
-echo "Installed $(nvim --version)"
-
-brew install ngrok/ngrok/ngrok
-echo "Installed $(ngrok -v)"
-
-brew install starship
-echo "Installed $(starship -V)"
+ln -s ~/dotfiles/nvim ~/.config/nvim
